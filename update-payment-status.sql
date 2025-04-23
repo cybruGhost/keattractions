d40452de776b7d8dd568650597f60550af1d0079
cyberghost <113809799+cybruGhost@users.noreply.github.com>
2025-04-23 19:08:43 +0300

    Update update-payment-status.sql

--- update-payment-status.sql
+++ update-payment-status.sql
@@ -1,4 +1,4 @@
--- Modify the payment_status column to include 'partially_paid'
+--i am Modifying the payment_status column to include 'partially_paid'
 ALTER TABLE bookings 
 MODIFY COLUMN payment_status ENUM('unpaid', 'paid', 'refunded', 'partially_paid') DEFAULT 'unpaid';
 
